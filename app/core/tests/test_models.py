a37bda655589ffa39e8a3d94ddd5a7e3a75cf717
nimrodrozsa <nrozsa@stanleybet.ro>
2021-08-16 18:43:55 +0300

    Added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -62,3 +62,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe model representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom auce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
