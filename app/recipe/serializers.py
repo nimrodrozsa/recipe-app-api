0b4389217dcd210430f6ad54309c596650c3d7ba
nimrodrozsa <nrozsa@stanleybet.ro>
2021-08-20 14:32:53 +0300

    Added image upload to recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,3 +1,4 @@
+from django.forms import fields
 from rest_framework import serializers
 
 from core.models import Tag, Ingredient, Recipe
@@ -43,3 +44,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes"""
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'image')
+        read_only_fields = ('id', )
